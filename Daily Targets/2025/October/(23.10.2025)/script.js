f46d543c28f67a9721d0e215d0907d58630fe048
Winterr <97064959+Winterr4077@users.noreply.github.com>
2025-10-23 22:14:36 +0100

    Create script.js

---
+++ Daily Targets/2025/October/(23.10.2025)/script.js
@@ -0,0 +1,8 @@
+<a></a><i></i><b></b>
+<style>
+body{margin:0;background:#FADE8B}
+a,i,b{position:fixed;background:#414B81}
+a{width:40px;height:200px;top:50px;left:120px;box-shadow:120px 0 #414B81}
+i{width:200px;height:40px;top:90px;left:100px;box-shadow:0 80px #414B81}
+b{width:40px;height:120px;top:90px;left:180px}
+</style>
